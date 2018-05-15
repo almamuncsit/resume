69d92054b55b7006662ec0f4de3206cc58b8ece1
Al-Mamun Sarkar <almamun.csit@gmail.com>
2018-05-16 02:52:48 +0600

    Updated settings

--- resume/settings.py
+++ resume/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL='/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT='/home/msarkar/www/resume/static/'
 MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT='/home/msarkar/www/resume/media/'
