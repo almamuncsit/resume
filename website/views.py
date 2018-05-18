e680305f14aea4e8068882e0225568477ac4407c
Al-Mamun Sarkar <almamun.csit@gmail.com>
2018-05-18 22:37:07 +0600

    Updated design

--- website/views.py
+++ website/views.py
@@ -7,8 +7,8 @@ def home(request):
     data = {}
     data['about'] = About.objects.all()[0]
     data['social'] = Social.objects.all()[0]
-    data['experiences'] = Experience.objects.all()
-    data['educations'] = Education.objects.all()
+    data['experiences'] = Experience.objects.order_by("-id").all()
+    data['educations'] = Education.objects.order_by("-id").all()
     data['skillCategories'] = SkillCategory.objects.order_by("order").all()
     return render(request, 'index.html', data)
 
